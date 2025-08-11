1401d446d2b3cae6907094da6e7c4320ab0dd60a
al1prahman <rahmanalifm21@gmail.com>
2025-08-11 10:29:55 +0700

    add env and firebase config

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,13 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCBp9eEJIQ25usW77Mu_QDwSVx5q4fDHTU",
-  authDomain: "inner-core-app.firebaseapp.com",
-  projectId: "inner-core-app",
-  storageBucket: "inner-core-app.firebasestorage.app",
-  messagingSenderId: "785160615489",
-  appId: "1:785160615489:web:97cb9bad56de846c5178c0"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
